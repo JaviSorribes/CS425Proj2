bdf1034376a3d84e2682807107cf71104f2a62aa
Javier Sorribes Camargo <javiersorribes@hotmail.es>
2017-04-25 03:06:11 -0500

    Implement Manage students for teacher

--- app.py
+++ app.py
@@ -161,7 +161,10 @@ def sqlcommands():
             cursor.execute(query)
             course_schema = ['semester']
             return [tup2dict(tup, course_schema) for tup in cursor.fetchall()]
-
+        def studentsforteacher(teacherid):
+            query = "SELECT * FROM student s NATURAL JOIN (SELECT * FROM takes NATURAL JOIN (SELECT * FROM course WHERE teacherid={}) c) t ORDER BY t.name, t.year, t.semester, s.studentid".format(teacherid)
+            cursor.execute(query)
+            return [tup2dict(tup,schemas['student']+schemas['course']) for tup in cursor.fetchall()]
         def year():
             query = "SELECT DISTINCT(year) from COURSE"
             cursor.execute(query)
